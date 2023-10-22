f3a8bc8eb241adb7b107dfb8a8b690a36afd9a31
SuNnY BiByAn <sunnykumar6121997@gmail.com>
2023-10-22 09:41:58 +0530

    first commit

---
+++ loops.js
@@ -0,0 +1,30 @@
+//print Hellop World 100 times
+
+for(let count=1; count<=100; count++)
+{
+    console.log("Hello World");
+}
+
+//print number 1 to 100
+for(let count=1; count<=100; count++)
+{
+    console.log(count);
+}
+
+//print odd number 1 to 100
+
+for(let count=1; count<=100; count+=2)
+{
+    console.log(count);
+}
+
+//print 1 to 100 multiplication
+
+for(let count = 1; count<=20; count*=2){
+    console.log(count);
+}
+
+for(let count=100; count>=1; count--)
+{
+    console.log(count);
+}
---
+++ script.js
@@ -0,0 +1,132 @@
+// alert("hello javascript");
+// document.getElementById().innerHTML="Hello javascript";
+
+
+
+// Problem 1: ----------------------------
+
+// let a = 5; //First variable
+// let b = 7; //2nd variable
+// let s = a + b; //sum of variable
+// document.write(s);
+
+
+
+// Problem 2: ----------------------------
+
+
+// var a = 10
+ 
+// document.write("Before Increment :", a);
+
+//  a += 5;
+// document.write("<br>");
+// document.write("After Increment :", a)
+
+// var size = 5;
+// var a = 5;
+// var size = 4;
+// for(let i =size; i>=0; i--)
+// {
+
+//     console.log(a);
+//     a=a-2;
+// 
+
+// var a= 0;
+// for(a; a<5; a++);
+// console.log(a);
+
+
+// for(var p in o)
+// console.log(o[p]);
+
+// function range(length)
+// {
+//     var a=5;
+//     for(var i=0; i<length; i++)
+//     {
+//         document.write(a);
+//     }
+// }
+// range(3);
+
+// var a = 10;
+// do{
+//     a+=1;
+//     console.log(a);
+// }while(a<5);
+
+// var a = 0;
+// var b = 0;
+// while(a<3)
+// {
+//     a++
+//     b+=a;
+//     console.log(b);
+// }
+
+
+
+// var length = 5;
+// var a = 5;
+// var length = 4;
+// do
+// {
+//     console.log(a);
+//     a=a-2;
+//     length--;
+// }while(length<0);
+
+
+
+// function print(length)
+// {
+//     var num = 3;
+//     do
+//     {
+//         document.write(num);
+//         length++;
+
+//     }while(length>0);
+// }
+// print(5);
+
+
+// let n = 5;
+// let string = "";
+// for(let i = 1; i <= n; i++){
+// for(let j = 1; j <= n - i+1; j++){
+//         string += n - j - i + 2;
+//     }
+//     string += "\n";
+// }
+// console.log(string);
+
+
+// function hypotenuse(a,b)
+// {
+//     function square(X)
+//     {
+//         return X*X;
+//     }
+//     return Math.sqrt(square(a)+ square(b));
+// }
+
+
+// SunnY
+
+// function greet(name){
+//     console.log("hello " + name + ":)" )
+// }
+
+// let name = prompt("Enter a name :");
+
+// greet(name);
+
+function add(a,b){
+
+    console.log(a+b);
+}
+
+add(2,3)
